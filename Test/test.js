fec904d89bdbef2f00901c305321272d4f425831
Daymorelah <demola.hussainin@gmail.com>
2017-07-02 17:08:43 +0100

    Edit files to be Runtime error free

--- Test/test.js
+++ Test/test.js
@@ -3,26 +3,26 @@
 //You can also do "var assert = require ('assert');//this uses nodes's implicit assert module. "
 //const assert = require ('chai').assert; //we need the assert library from chai
 //const myScript = require ('../src/factorial.js') ;
-import assert from 'chai'
+const assert = require('chai').assert
 import factorial from '../src/factorial'
 
 describe('Factorial Function', ()=>{
 	
 	describe('Invalid Input', ()=>{
 		it('should return undefined for inputs less than zero', ()=>{
-			assert.equal(myScript(-1),'undefined');
+			assert.equal(factorial(-1),'undefined');
 		}); //end of it block 
 		it('should return 1 for inputs equal to one', ()=>{
-			assert.equal(myScript(1),1);
+			assert.equal(factorial(1),1);
 		}); //end of it block 
 		it('should return not an integer for inputs that are not integers', ()=>{
-			assert.equal(myScript('well'),'You entered a string.');
+			assert.equal(factorial('well'),'You entered a string.');
 		}); //end of it block 
 	}); // end of inner test suite
 
 	describe('Valid Input', ()=>{
 		it('should return 120 for input of 5',()=>{
-			assert.equal(myScript(5),120);
+			assert.equal(factorial(5),120);
 		}); //end of it block 
 		}); //end of secind inner test suite
 }); // end of outter test suite
