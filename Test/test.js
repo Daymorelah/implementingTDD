2fdc449820d65a15898610f55bb1507ecc956391
Daymorelah <demola.hussainin@gmail.com>
2017-07-02 10:13:28 +0100

    Add more invalid test cases to the test.js file and Add code logic to implement these added test

--- Test/test.js
+++ Test/test.js
@@ -6,7 +6,7 @@ var myScript = require ('../src/factorial.js') ;
 describe('Factorial Function', function(){
 	describe('Invalid Input', function(){
 		it('should return undefined for inputs less than zero', function(){
-			assert.equal(myScript.factorial(-1),'undefined');
+			assert.equal(myScript.anyVarName(-1),'undefined');
 		}); //end of it block 
 	}); // end of inner test suite
 }); // end of outter test suite
--- src/factorial.js
+++ src/factorial.js
@@ -2,8 +2,8 @@
 
 'use strict' ; // confines javscript to a strict version
 
-module.exports={
-	factorial: function(number){
+var factorial = function (number) { //function statement
+
 	if (number<0) {
 		return 'undefined'
 	};//end of if statement
@@ -14,4 +14,32 @@ module.exports={
 	return result ;
 } // end of fucntion factorial
 
-}
+module.exports = factorial
+//Note: you can also do: exports.anyVarName = factorial; but in test.js u call it as 'myScritp.anyVarName()'.
+
+/*You can also export objects via d obect method as show below
+var factorial ={}; //declare an empty object
+factorial.factorial =  function (number) { 	//using function statement
+	if (number<0) {
+		return 'undefined'
+	};//end of if statement
+	let result = 1 ;
+	for (let i = number; i<=1; i--){
+		result = result*i ;
+	} // end of for loop
+	return result ;
+} // end of fucntion factorial
+
+module.exports = factorial;
+then in the test.js u call d function as 'myScript.factorial()'
+
+You can also do thus: 
+var factorial = {
+	factorial : function(number){
+	....Code goes here!
+	}
+}; // end of factorial object
+module.exports = factorial;
+u call the function in test.js as 'myScript.factorial()'
+
+*/
